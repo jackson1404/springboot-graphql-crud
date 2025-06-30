f6d7bbe35c219dbf4e3ab2fff5d3765c0456d9f5
jackson1404 <jackson14042004@gmail.com>
2025-06-30 22:38:50 +0630

    first commit

---
+++ src/test/java/com/jackson/springboot_graphql_crud/SpringbootGraphqlCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackson.springboot_graphql_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGraphqlCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
